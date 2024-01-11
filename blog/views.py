65ceccdbf53044308f2b6da25e47feabca46a78e
Julius Charles <cjmatheka@gmail.com>
2024-01-11 09:08:54 +0300

    add blog app to the website. The blog have will have a blog home and blog about us page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(requests):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+def about(requests):
+    return HttpResponse('<h1> Blog About Us</h1>')
