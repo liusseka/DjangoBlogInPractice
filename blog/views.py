b82cd0d98608f5874c5de0846a421e42dd2e47eb
Julius Charles <cjmatheka@gmail.com>
2024-01-11 11:57:37 +0300

    Update the blog app for the Django website. The update includes creating html and css files, statics and templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Julius Charles',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'January 11, 2024'
+    },
+    {
+        'author': 'Kennedy Charles',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'January 12, 2024'
+    }
+]
+
 
 def home(requests):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(requests, 'blog/home.html', context)
+
 
 def about(requests):
-    return HttpResponse('<h1> Blog About Us</h1>')
+    return render(requests, 'blog/about.html', {'title': 'About'})
